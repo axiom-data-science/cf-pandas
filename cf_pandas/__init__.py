fd8473742fa79c366ee753db793fdf32c731570e
Kristen Thyng <kthyng@gmail.com>
2022-11-03 13:45:23 -0500

    all of Reg class

--- cf_pandas/__init__.py
+++ cf_pandas/__init__.py
@@ -6,6 +6,7 @@ from pkg_resources import DistributionNotFound, get_distribution
 
 from .accessor import CFAccessor  # noqa
 from .options import set_options  # noqa
+from .reg import Reg
 from .utils import always_iterable, astype, match_criteria_key
 
 
